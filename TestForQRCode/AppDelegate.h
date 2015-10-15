26f516e76a9e3f35c7c072786959eba7a8fe8163
shangzhixin <shangzhixin@sumaplayer.com>
2015-10-15 10:01:11 +0800

    Initial Commit

---
+++ TestForQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForQRCode
+//
+//  Created by new on 15-10-15.
+//  Copyright (c) 2015年 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForQRCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestForQRCode
+//
+//  Created by new on 15-10-15.
+//  Copyright (c) 2015年 new. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
